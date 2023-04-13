ee0d77ee86b111870acf2f5dce522e393307158c
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-13 15:40:30 +0100

    data section added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Data from './components/data/Data';
 
 function App() {
   return (
        <>
         <Navbar />
         <Hero />
+        <Data />
        </>
   );
 }
---
+++ src/components/data/Data.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DataStyles.css'
+
+const Data = () => {
+  return (
+    <div className='data'>
+      <div className="container">
+        <div className="content">
+            <h2><span>Data</span> Recovery</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam suscipit sunt fugit ex accusamus esse ut impedit, corrupti ad aspernatur tenetur eveniet ipsa eligendi nobis praesentium earum et dolores commodi!</p>
+            <div>
+                <button>Sign Up</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Data
