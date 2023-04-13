6e4cde195e3517c2927b0f20361a9ca5ed4dc262
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-13 16:46:06 +0100

    cloud section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Data from './components/data/Data';
+import Cloud from './components/cloud/Cloud';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Hero />
         <Data />
+        <Cloud />
        </>
   );
 }
---
+++ src/components/cloud/Cloud.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './CloudStyles.css'
+
+const Cloud = () => {
+  return (
+    <div className='cloud'>
+        <div className="container">
+            <div className="content">
+                <h2><span>Cloud</span> Security</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus perferendis esse maiores? Accusantium non rerum consequatur eveniet et voluptates temporibus saepe, quas dolore modi expedita perferendis praesentium, adipisci reprehenderit?</p>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Cloud
