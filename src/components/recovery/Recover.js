ca9143d98ccdfd358452005239c9073fd8de1e9f
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-16 12:38:47 +0100

    additional changes

--- src/components/recovery/Recover.js
+++ src/components/recovery/Recover.js
@@ -7,7 +7,7 @@ const Recover = () => {
       <div className="container">
         <div className="content">
           <h2><span>Data</span> Recovery</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis velit suscipit cumque, eius vel, nam distinctio sapiente obcaecati repellat ex excepturi tempore ratione unde possimus! Nam, sed officia. Minima, sit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis velit suscipit cumque, eius vel, nam distinctio sapiente obcaecati repellat ex excepturi tempore ratione unde possimus! Nam, sed officia.Lorem ipsum dolor sit amet.</p>
           <button>Learn More</button>
         </div>
         
